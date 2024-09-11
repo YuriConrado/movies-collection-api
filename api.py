b56961757383a96bcbc2150ca01db55b2239c0d8
YuriConrado <yuriconradosantos123@gmail.com>
2024-09-11 12:24:59 -0300

    api

--- api.py
+++ api.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
 db = SQLAlchemy(app)
 
-class Movie(db.Model): 
-    id = db.Column(db.Integer, primary_key=True)             
-    title = db.Column(db.String(100), nullable=False) 
-    genre = db.Column(db.String(100), nullable=False)  
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    genre = db.Column(db.String(100), nullable=False)
 
 with app.app_context():
     db.create_all()
@@ -16,12 +16,12 @@ with app.app_context():
 @app.route('/api/movies', methods=['GET'])
 def get_movies():
     movies = Movie.query.all()
-    return jsonify([{"id":movie.id, "title":movie.title, "genre":movie.genre}for movie in movies])
+    return jsonify([{"id":movie.id,"title":movie.title,"genre":movie.genre} for movie in movies])
 
 @app.route('/api/movies/<int:id>', methods=['GET'])
 def get_movie(id):
     movie = Movie.query.get(id)
-    return jsonify({"id":movie.id, "title":movie.title, "genre":movie.genre}) if movie else ('', 404)
+    return jsonify({"id":movie.id,"title":movie.title,"genre":movie.genre}) if movie else ('', 404)
 
 @app.route('/api/movies', methods=['POST'])
 def add_movie():
@@ -29,30 +29,34 @@ def add_movie():
     new_movie = Movie(title=data['title'], genre=data['genre'])
     db.session.add(new_movie)
     db.session.commit()
-    return jsonify({"id":new_movie.id, "title":new_movie.title, "genre":new_movie.genre}), 201
+    return jsonify({"id":new_movie.id,"title":new_movie.title,"genre":new_movie.genre}), 201
 
 @app.route('/api/movies/<int:id>', methods=['DELETE'])
 def delete_movie(id):
     movie = Movie.query.get(id)
     if movie is None:
-        return jsonify({"error": "movie not found"}), 404
-    
+        return jsonify({"error":"movie not found"}), 404
+
     db.session.delete(movie)
     db.session.commit()
     return '', 204
 
 @app.route('/api/movies/<int:id>', methods=['PUT'])
-def uptade_movie(id):
+def update_movie(id):
     movie = Movie.query.get(id)
     if movie is None:
-        return jsonify({"error": "movie not found"}), 404
+        return jsonify({"error":"movie not found"}), 404
     
     data = request.get_json()
-    movie.title = data.get('title','movie.title')
-    movie.genre = data.get('genre','movie.genre')
+    movie.title = data.get('title', movie.title)
+    movie.genre = data.get('genre', movie.genre)
 
     db.session.commit()
-    return jsonify({"id":movie.id, "title":movie.title, "genre":movie.genre})
+    return jsonify({
+        "id":movie.id,
+        "title":movie.title,
+        "genre":movie.genre
+        })
 
-if __name__ == 'main':
+if __name__ == '__main__':
     app.run(port=5000, debug=True)
